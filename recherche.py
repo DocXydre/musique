2fe9bff1dd6e2497a2a09ba133e3197343f32edf
DocXydre <74959654+DocXydre@users.noreply.github.com>
2021-02-13 11:57:36 +0100

    ya

--- recherche.py
+++ recherche.py
@@ -10,7 +10,8 @@ Fenetre.geometry('1920x1080')
 
 fond = PhotoImage(file='menu.gif')
 
-
+pygame.init()
+intro=pygame.mixer.Sound("skyrock.wav")
 
 LabPay=Label(Fenetre,image=fond)#image de fond
 LabPay.pack()
@@ -19,7 +20,7 @@ def traiter():
     mes1=ISBN.get()
     print(mes1)
 
-
+intro.play
 blaze= tk.Entry(Fenetre, textvariable="vald", width=30,font='Impact 13')
 blaze.place(x=220,y=250)
 nom= tk.Entry(Fenetre, textvariable="lezarman", width=30,font='Impact 13')
@@ -27,5 +28,5 @@ nom.place(x=220,y=350)
 Bouttonsuivant = Button(Fenetre,text="Suite",height=1,command=traiter,font='Impact 12',fg='white',bg='red',cursor='hand2')#boutton pour passer à la page suivante
 Bouttonsuivant.place(x=220,y=460)
 
-
+pygame.quit()
 Fenetre.mainloop()
