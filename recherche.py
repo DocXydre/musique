1a6fd6611905b00114be9bfae0a727c9c728c9d6
DocXydre <74959654+DocXydre@users.noreply.github.com>
2021-02-13 13:10:50 +0100

    y

--- recherche.py
+++ recherche.py
@@ -4,15 +4,12 @@ import tkinter as tk
 import os
 import pygame
 
+
 Fenetre=Tk()
 Fenetre.title("Saucisse")
 Fenetre.geometry('1920x1080')
 
 fond = PhotoImage(file='menu.gif')
-
-pygame.init()
-intro=pygame.mixer.Sound("skyrock.wav")
-
 LabPay=Label(Fenetre,image=fond)#image de fond
 LabPay.pack()
 
@@ -20,13 +17,18 @@ def traiter():
     mes1=ISBN.get()
     print(mes1)
 
-intro.play()
+
 blaze= tk.Entry(Fenetre, textvariable="vald", width=30,font='Impact 13')
 blaze.place(x=220,y=250)
 nom= tk.Entry(Fenetre, textvariable="lezarman", width=30,font='Impact 13')
 nom.place(x=220,y=350)
 Bouttonsuivant = Button(Fenetre,text="Suite",height=1,command=traiter,font='Impact 12',fg='white',bg='red',cursor='hand2')#boutton pour passer à la page suivante
 Bouttonsuivant.place(x=220,y=460)
+
+pygame.init()
+intro=pygame.mixer.Sound("skyrock.wav")
+intro.play(loops=-1, maxtime=0, fade_ms=0)
+bug.set_volume(0.25)
 
 pygame.quit()
 Fenetre.mainloop()
